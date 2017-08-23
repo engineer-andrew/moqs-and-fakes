722f0d57b95d896765fbc1dada1d32591e39b745
Andrew Webster <andrew.webster@heatsoftware.com>
2017-08-23 13:48:22 -0700

    Added unit tests, but nothing moq'd yet

---
+++ MoqsAndFakes.CardGameSuite.Utilities.UnitTests/Shuffler.Tests.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using Xunit;
+
+namespace MoqsAndFakes.CardGameSuite.Utilities.UnitTests
+{
+    public class ShufflerTests : IDisposable
+    {
+        private readonly Shuffler _shuffler;
+
+        public ShufflerTests()
+        {
+            _shuffler = new Shuffler();
+        }
+
+        [Fact]
+        public void ShuffleShouldShuffleSingleDeckOfCards()
+        {
+            // arrange
+            var originalDeck = new List<string>
+            {
+                "2S", "3S", "4S", "5S", "6S", "7S", "8S", "9S", "10S", "JS", "QS", "KS", "AS",
+                "2H", "3H", "4H", "5H", "6H", "7H", "8H", "9H", "10H", "JH", "QH", "KH", "AH",
+                "2C", "3C", "4C", "5C", "6C", "7C", "8C", "9C", "10C", "JC", "QC", "KC", "AC",
+                "2D", "3D", "4D", "5D", "6D", "7D", "8D", "9D", "10D", "JD", "QD", "KD", "AD"
+            };
+
+            // act
+            var result = _shuffler.Shuffle(1);
+
+            // assert
+            Assert.NotEqual(originalDeck, result);
+        }
+
+        [Fact]
+        public void ShuffleShouldShuffleMultipleDecksOfCards()
+        {
+            // arrange
+            var originalDeck = new List<string>
+            {
+                "2S", "3S", "4S", "5S", "6S", "7S", "8S", "9S", "10S", "JS", "QS", "KS", "AS",
+                "2H", "3H", "4H", "5H", "6H", "7H", "8H", "9H", "10H", "JH", "QH", "KH", "AH",
+                "2C", "3C", "4C", "5C", "6C", "7C", "8C", "9C", "10C", "JC", "QC", "KC", "AC",
+                "2D", "3D", "4D", "5D", "6D", "7D", "8D", "9D", "10D", "JD", "QD", "KD", "AD"
+            };
+            var multipleDecks = new List<string>();
+            multipleDecks.AddRange(originalDeck);
+            multipleDecks.AddRange(originalDeck);
+            multipleDecks.AddRange(originalDeck);
+            multipleDecks.AddRange(originalDeck);
+            multipleDecks.AddRange(originalDeck);
+            multipleDecks.AddRange(originalDeck);
+            multipleDecks.AddRange(originalDeck);
+            multipleDecks.AddRange(originalDeck);
+
+            // act
+            var result = _shuffler.Shuffle(8);
+
+            // assert
+            Assert.NotEqual(multipleDecks, result);
+        }
+
+        [Fact]
+        public void ShuffleShouldShuffleCardsInDifferentOrderEveryTime()
+        {
+            // arrange
+            var firstRun = _shuffler.Shuffle(8);
+
+            // act
+            var result = _shuffler.Shuffle(8);
+
+            // assert
+            Assert.NotEqual(firstRun, result);
+        }
+
+        public void Dispose()
+        {
+        }
+    }
+}
---
+++ MoqsAndFakes.CardGameSuite.Utilities/Shuffler.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace MoqsAndFakes.CardGameSuite.Utilities
+{
+    public class Shuffler
+    {
+        private readonly List<string> _unshuffledDeck = new List<string>
+        {
+            "2S", "3S", "4S", "5S", "6S", "7S", "8S", "9S", "10S", "JS", "QS", "KS", "AS",
+            "2H", "3H", "4H", "5H", "6H", "7H", "8H", "9H", "10H", "JH", "QH", "KH", "AH",
+            "2C", "3C", "4C", "5C", "6C", "7C", "8C", "9C", "10C", "JC", "QC", "KC", "AC",
+            "2D", "3D", "4D", "5D", "6D", "7D", "8D", "9D", "10D", "JD", "QD", "KD", "AD"
+        };
+
+        public List<string> Shuffle(int numberOfDecks)
+        {
+            var deck = new List<string>();
+
+            while (deck.Count < numberOfDecks * 52)
+            {
+                var random = RollDice(52);
+                var card = _unshuffledDeck[random - 1];
+
+                var numberOfInstancesOfCard = deck.Count(c => c == card);
+
+                if (numberOfInstancesOfCard < numberOfDecks)
+                {
+                    deck.Add(card);
+                }
+            }
+
+            return deck;
+        }
+
+        private static byte RollDice(byte numberSides)
+        {
+            var rngCsp = RandomNumberGenerator.Create();
+            if (numberSides <= 0)
+                throw new ArgumentOutOfRangeException("numberSides");
+
+            // Create a byte array to hold the random value.
+            byte[] randomNumber = new byte[1];
+            do
+            {
+                // Fill the array with a random value.
+                rngCsp.GetBytes(randomNumber);
+            }
+            while (!IsFairRoll(randomNumber[0], numberSides));
+            // Return the random number mod the number
+            // of sides.  The possible values are zero-
+            // based, so we add one.
+            return (byte)((randomNumber[0] % numberSides) + 1);
+        }
+
+        private static bool IsFairRoll(byte roll, byte numSides)
+        {
+            // There are MaxValue / numSides full sets of numbers that can come up
+            // in a single byte.  For instance, if we have a 6 sided die, there are
+            // 42 full sets of 1-6 that come up.  The 43rd set is incomplete.
+            int fullSetsOfValues = Byte.MaxValue / numSides;
+
+            // If the roll is within this range of fair values, then we let it continue.
+            // In the 6 sided die case, a roll between 0 and 251 is allowed.  (We use
+            // < rather than <= since the = portion allows through an extra 0 value).
+            // 252 through 255 would provide an extra 0, 1, 2, 3 so they are not fair
+            // to use.
+            return roll < numSides * fullSetsOfValues;
+        }
+    }
+}
